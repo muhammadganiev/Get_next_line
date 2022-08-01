369baf582989a1a64c0b817ab203125672a0c837
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-08-01 17:56:48 +0400

    pre test

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,129 @@
 /*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 23:31:37 by muganiev          #+#    #+#             */
-/*   Updated: 2022/07/20 19:38:06 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/08/01 13:49:15 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd);
+char	*ft_before(char *str)
 {
-	
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (str[0] == '\0')
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * i + 2);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\n' && str[i])
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		ptr[i++] = '\n';
+	ptr[i] = '\0';
+	return (ptr);
 }
 
-int	f1(int *j)
+char	*ft_after(char *str)
 {
-	*j = *j * 2;
-	return (*j);
+	int		i;
+	int		j;
+	char	*ptr;
+
+	j = 0;
+	if (!str)
+		return (NULL);
+	i = ft_strlen(str);
+	while (str[j] != '\n' && str[j])
+		j++;
+	if (str[j] == '\0')
+	{
+		free(str);
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * (i - j));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	j++;
+	while (str[j])
+		ptr[i++] = str[j++];
+	ptr[i] = '\0';
+	free(str);
+	return (ptr);
 }
 
-int	main(void)
+int	has_newline(char *str)
 {
-	int	i;
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
 
-	i = 5;
-	f1(&i);
-	printf("%d\n", i);
+char	*ft_read(int fd, char *buf, char *tmp, char *str)
+{
+	int		i;
+
+	i = 1;
+	while (i != 0)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free (buf);
+			return (NULL);
+		}
+		buf[i] = '\0';
+		tmp = str;
+		if (!tmp)
+		{
+			tmp = malloc(sizeof(char) * 1);
+			tmp[0] = '\0';
+		}
+		str = ft_strjoin(tmp, buf);
+		free(tmp);
+		if (has_newline(str) == 1)
+			break ;
+	}
+	free(buf);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*buf;
+	char		*line;
+	char		*tmp;
+
+	tmp = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	str = ft_read(fd, buf, tmp, str);
+	if (!str)
+		return (NULL);
+	line = ft_before(str);
+	str = ft_after(str);
+	return (line);
 }
---
+++ tests/main.c
@@ -0,0 +1,94 @@
+#include "../get_next_line.h"
+#include <stdio.h>
+
+#define NO_OF_FILES 1
+// linux systems limit the num of FD that any one process may open to 1024 per process.
+#define MAX_FD 1024
+
+int	main(void)
+{
+	int		fd, fd2;
+	int		i;
+	char	*line[MAX_FD];
+	char	*arr_of_filenames[] = {
+		// "gnlTester/files/big_line_no_nl",
+		"tests/test2.txt",
+		"tests/sample.txt",
+		"tests/test.txt",
+	};
+	int		file_counter;
+
+	// fd_1 = open("tests/J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt", \
+	// 				O_RDONLY);
+	file_counter = 0;
+	while (file_counter < NO_OF_FILES)
+	{
+		i = 1;
+		fd = open(arr_of_filenames[file_counter], O_RDONLY);
+		printf("----------------------------\n");
+		printf("Filename: %s\n", arr_of_filenames[file_counter]);
+		while (1)
+		{
+			line[fd] = get_next_line(fd);
+			if (!line[fd])
+				break;
+			else
+				printf("Line %d for fd %d: %s", i, fd, line[fd]);
+			free(line[fd]);
+			i++;
+		}
+		printf("\n----------------------------\n");
+		close(fd);
+		file_counter++;
+	}
+	// file_counter = 0;
+	// i = 0;
+	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("----------------------------\n");
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// 	line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// close(fd);
+	// printf("\n");
+	// // i= 0;
+	// // file_counter++;
+	// // fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// // printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// // line[fd] = get_next_line(fd);
+	// // printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// // free(line[fd]);
+
+	// i = 0;
+	// file_counter = 0;
+	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// free(line[fd]);
+
+	// file_counter++;
+	// fd2 = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd2] = get_next_line(fd2);
+	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
+	// free(line[fd2]);
+
+	// i = 0;
+	// file_counter = 0;
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// free(line[fd]);
+
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd2] = get_next_line(fd2);
+	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
+	// free(line[fd2]);
+}
