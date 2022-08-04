6288e7fb830960dd0cea1be565a92e1b3cf0abc2
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-08-04 19:50:40 +0400

    tests

--- tests/main.c
+++ tests/main.c
@@ -1,95 +1,48 @@
-#include "../get_next_line.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muganiev <gf.black.tv@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 18:51:05 by muganiev          #+#    #+#             */
+/*   Updated: 2022/08/04 19:34:21 by muganiev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define NO_OF_FILES 1
-#define MAX_FD 1024
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../libft/libft.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buf[15];
+	char		*line;
+	int			i;
+
+	i = 0;
+	if (buf[i] == '\0')
+	{
+		read (fd, buf, 14);
+	}
+	buf[14] = '\0';
+	while (buf[i] && buf[i] != ' ')
+	{
+		printf("%c", buf[i]);
+		i++;
+	}
+	printf("\n");
+	return (buf);
+}
 
 int	main(void)
 {
 	int		fd;
-	int		fd2;
-	int		i;
-	char	*line[MAX_FD];
-	char	**arr_of_filenames;
-	*arr_of_filenames[]= {
-		// "gnlTester/files/big_line_no_nl",
-		"tests/test2.txt",
-		"tests/sample.txt",
-		"tests/test.txt",
-	};
-	int		file_counter;
 
-	// fd_1 = open("tests/J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt", \
-	// 				O_RDONLY);
-	file_counter = 0;
-	while (file_counter < NO_OF_FILES)
-	{
-		i = 1;
-		fd = open(arr_of_filenames[file_counter], O_RDONLY);
-		printf("----------------------------\n");
-		printf("Filename: %s\n", arr_of_filenames[file_counter]);
-		while (1)
-		{
-			line[fd] = get_next_line (fd);
-			if (!line[fd])
-				break;
-			else
-				printf("Line %d for fd %d: %s", i, fd, line[fd]);
-			free(line[fd]);
-			i++;
-		}
-		printf("\n----------------------------\n");
-		close(fd);
-		file_counter++;
-		}
-	// file_counter = 0;
-	// i = 0;
-	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
-	// printf("----------------------------\n");
-	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// line[fd] = get_next_line(fd);
-	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
-	// free(line[fd]);
-	// line[fd] = get_next_line(fd);
-	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
-	// free(line[fd]);
-	// 	line[fd] = get_next_line(fd);
-	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
-	// free(line[fd]);
-	// close(fd);
-	// printf("\n");
-	// // i= 0;
-	// // file_counter++;
-	// // fd = open(arr_of_filenames[file_counter], O_RDONLY);
-	// // printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// // line[fd] = get_next_line(fd);
-	// // printf("Line %d for fd %d: %s", i, fd, line[fd]);
-	// // free(line[fd]);
-
-	// i = 0;
-	// file_counter = 0;
-	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
-	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// line[fd] = get_next_line(fd);
-	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
-	// free(line[fd]);
-
-	// file_counter++;
-	// fd2 = open(arr_of_filenames[file_counter], O_RDONLY);
-	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// line[fd2] = get_next_line(fd2);
-	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
-	// free(line[fd2]);
-
-	// i = 0;
-	// file_counter = 0;
-	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// line[fd] = get_next_line(fd);
-	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
-	// free(line[fd]);
-
-	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
-	// line[fd2] = get_next_line(fd2);
-	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
-	// free(line[fd2]);
+	fd = open("text.txt", O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
 }
